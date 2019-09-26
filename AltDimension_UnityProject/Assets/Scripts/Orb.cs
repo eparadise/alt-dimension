ad07ada3a16eda9fef7728f7ca4eebd8943e4bc5
Delaney Sears <delaneysears@G223-1659.DisStu.local>
2019-09-26 10:28:12 +0200

    message- orbz

---
+++ AltDimension_UnityProject/Assets/Scripts/Orb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Author: Delaney Sears
+public class Orb : MonoBehaviour
+{
+
+    float orbCount;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gainOrb()
+    {
+        orbCount++;
+        Destroy(gameObject);
+    }
+}
