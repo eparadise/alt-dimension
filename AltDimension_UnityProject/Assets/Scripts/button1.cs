df484eb93f390d7cc1ecab6128314da087c194fb
Delaney Sears <delaneysears@G223-1679.DisStu.local>
2019-09-27 10:18:50 +0200

    buttons somewhat

---
+++ AltDimension_UnityProject/Assets/Scripts/button1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button1 : MonoBehaviour
+{
+    
+    public static void nextIntro()
+    {
+        SceneManager.LoadScene("IntroScene2");
+    }
+
+    
+}
