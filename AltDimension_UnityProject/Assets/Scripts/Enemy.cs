e7e0db091ef344047e58e4559bd44cce6e63452a
Bea Rose <bearose12@gmail.com>
2019-09-27 10:30:29 +0200

    snake updates

--- AltDimension_UnityProject/Assets/Scripts/Enemy.cs
+++ AltDimension_UnityProject/Assets/Scripts/Enemy.cs
@@ -6,13 +6,14 @@ public class Enemy : MonoBehaviour
 {
     private Rigidbody2D myRb2D;
     private SpriteRenderer spriteRenderer;
-    //public Sprite spriteWiggle;
-    //public Sprite spriteFlat;
+    public Sprite spriteWiggle;
+    public Sprite spriteFlat;
     private Vector3 moveDirection = Vector3.forward;
 
-    private float timeLeft = 3.0f;
+    private float timeLeft = 1.5f;
     public float speed;
-    private bool swapSprite = true;
+    private bool wiggle = true;
+    private float spriteTimer = 0.25f;
     private bool switchDirections;
     private bool movingRight = true;
 
@@ -27,25 +28,31 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         timeLeft -= Time.deltaTime;
-        //switchDirections = false;
-        /* if (swapSprite)
+        spriteTimer -= Time.deltaTime;
+        
+        if (spriteTimer < 0)
         {
-            spriteRenderer.sprite = spriteWiggle;
-            swapSprite = false;
+            spriteTimer = 0.25f;
+            if(wiggle)
+            {
+                spriteRenderer.sprite = spriteWiggle;
+                wiggle = false;
+            }
+            else
+            {
+                spriteRenderer.sprite = spriteFlat;
+                wiggle = true;
+            }
         }
-        else
-        {
-            spriteRenderer.sprite = spriteFlat;
-            swapSprite = true;
-        } */
 
         Vector2 velocity = myRb2D.velocity;
 
         if (timeLeft < 0)
         {
-            timeLeft = 3.0f;
+            timeLeft = 1.5f;
             switchDirections = true;
         }
+
         if (switchDirections)
         {
             if(movingRight)
@@ -73,4 +80,6 @@ public class Enemy : MonoBehaviour
 
         myRb2D.velocity = velocity;
     }
+
+
 }
