b84c303012a2c6db664dfad9f14741c6fac29402
Delaney Sears <delaney.sears@gmail.com>
2019-10-01 11:22:34 +0200

    bear

---
+++ AltDimension_UnityProject/Assets/Scripts/Bear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour
+{
+    private SpriteRenderer mySR;
+    private Rigidbody2D myRb2d;
+    public Sprite angryBear;
+
+    void Start()
+    {
+        mySR = GetComponent<SpriteRenderer>();
+        myRb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            mySR.sprite = angryBear;
+        }
+    }
+}
