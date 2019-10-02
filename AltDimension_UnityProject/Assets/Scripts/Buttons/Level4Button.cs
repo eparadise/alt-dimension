7f68bead91625c32129f927fc5b22dce85f01092
Delaney Sears <delaney.sears@gmail.com>
2019-10-02 21:21:26 +0200

    menu

---
+++ AltDimension_UnityProject/Assets/Scripts/Buttons/Level4Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level4Button : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("DelaneyScene2");
+    }
+}
---
+++ AltDimension_UnityProject/Assets/Scripts/Buttons/Level5Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level5Button : MonoBehaviour
+{
+    public void Menu()
+    {
+        //SceneManager.LoadScene(name of 5th scene);
+    }
+}
