d19f130a0afaf14d128fcbb96e5f27fd5a7465a6
Delaney Sears <delaney.sears@gmail.com>
2019-10-02 13:16:44 +0200

    new buttons scripts

---
+++ AltDimension_UnityProject/Assets/Scripts/Buttons/Click3.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Click3 : MonoBehaviour
+{
+	public void NextIntro()
+	{
+		SceneManager.LoadScene("IntroScene4");
+	}
+}
